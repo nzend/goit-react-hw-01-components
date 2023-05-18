52b153158a7a02c2be063b0e253379ac6cd80499
nzend <115723110+nzend@users.noreply.github.com>
2023-05-18 20:49:19 +0300

    Add base styles and add Emotion

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,18 +3,19 @@ import { Statistics } from 'components/Statistics/Statistics';
 import { FriendList } from 'components/FriendList/FriendList';
 import { Transactions } from 'components/Transactions/Transactions';
 
-import user from '../user.json';
-import data from '../data.json';
-import friends from '../friends.json';
-import transactions from '../transactions.json';
+import user from '../../user.json';
+import data from '../../data.json';
+import friends from '../../friends.json';
+import transactions from '../../transactions.json';
 
+import { Container } from './App.styled';
 export const App = () => {
   return (
-    <div>
+    <Container>
       <ProfileBoard user={user}></ProfileBoard>
       <Statistics data={data}></Statistics>
       <FriendList friends={friends}></FriendList>
       <Transactions transactions={transactions}></Transactions>
-    </div>
+    </Container>
   );
 };
---
+++ src/components/App/App.styled.jsx
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  width: 1200px;
+  margin-left: auto;
+  margin-right: auto;
+  background-color: red;
+  padding: 50px;
+`;
